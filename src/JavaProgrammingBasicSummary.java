dc534b44c5962f9b099e15bd51d7e8fe1befbca3
kotoshin <yspm1127@gmail.com>
2024-06-18 21:23:02 +0900

    Javaプログラミング入門終了

---
+++ src/JavaProgrammingBasicSummary.java
@@ -0,0 +1,18 @@
+
+        public class JavaProgrammingBasicSummary {
+            // ~ 省略
+
+            // int型引数を2つ受け取るaddメソッドの定義（戻り値なし）
+            public static void add(int num1, int num2) {
+                int result = num1 + num2;
+                System.out.println("int型の加算：" + result);
+            }
+
+            // double型引数を2つ受け取るaddメソッドの定義（戻り値あり）
+            public static double add(double num1, double num2) {
+                double result = num1 + num2;
+                return result;
+            }
+
+
+   }
