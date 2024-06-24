bc1e22a9d27430bec6bce59619fba63f020125e8
kotoshin <yspm1127@gmail.com>
2024-06-24 18:03:30 +0900

    Lesson3課題再

--- src/Review01.java
+++ src/Review01.java
@@ -8,14 +8,12 @@ public class Review01 {
         int num1= 1500; //商品価格
         double num2= 0.1;  //消費税
       int num = tax(num1, num2);       
-      System.out.println(num1 + "円の商品の税込価格は" + (num1 + num) +  "円で消費税は" +num + "円です");
-        System.out.println(num);
+      System.out.println(num1 + "円の商品の税込価格は" + (num1 + num) +  "円（消費税は" +num + "円）です。");
         
     }
 
     private static int tax(int num1, double num2) {
         int num = 150;
-        //税込価格＝商品価格＋消費税
         return num;
       
 
