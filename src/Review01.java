597d862549459f9d4abd47b3975a0f0cef4ecdf4
kotoshin <yspm1127@gmail.com>
2024-06-25 01:46:31 +0900

    レッスン３再提出５

--- src/Review01.java
+++ src/Review01.java
@@ -1,14 +1,14 @@
 
 public class Review01 {
-    public static void main(String[] args) {
-        int num1= 1500; //商品価格
-        double num2= 0.1;  //消費税
-      int num = tax(num1, num2);
-        System.out.println(num1 + "円の商品の税込価格は" + (num1 + num) +  "円で消費税は" +num + "円です");
+        public static void main(String[] args) {
+            int num1= 1500; //商品価格
+            double num2= 0.1;  //消費税
+          int num = tax(num1, num2);
+            System.out.println(num1 + "円の商品の税込価格は" + (num1 + num) +  "円で消費税は" +num + "円です");
+        }
+        private static int tax(int num1, double num2) {
+            int num = 0;
+            num = (int) (num1 * num2);
+            return num;
+            }
     }
-    private static int tax(int num1, double num2) {
-        int num = 0;
-        num = (int) (num1 * num2);
-        return num;
-        }
-        }
