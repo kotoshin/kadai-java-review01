ebdb0f9b87e146b6023221a2d41fff6727adf107
kotoshin <yspm1127@gmail.com>
2024-06-28 11:49:44 +0900

    レッスン４

---
+++ src/IfSample2.java
@@ -0,0 +1,15 @@
+
+public class IfSample2 {
+
+    public static void main(String[] args) {
+                int score = 50; // 成績
+
+                // scoreが60未満でない ならば
+                if (!(score < 50)) {
+                    // 合格です！と表示
+                    System.out.println("合格です！");
+                }
+
+            }
+
+        }
---
+++ src/IfSample3.java
@@ -0,0 +1,16 @@
+
+public class IfSample3 {
+    public static void main(String[] args) {
+        int score = 44; // 成績
+
+        // scoreが60以上ならば
+        if (score >= 60) {
+            // 合格です！と表示
+            System.out.println("合格です！");
+        } else {
+            // 赤点です...と表示
+            System.out.println("赤点です...");
+        }
+    }
+
+}
